3623ad4bd661239f9dd0dd4cdf34e196073c2080
Stefan989732 <70566459+Stefan989732@users.noreply.github.com>
2022-01-23 14:46:33 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,25 +10,5 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         Game game = new Game();
         game.start();
-
-
-//        System.out.println(k);
-//        System.out.println(p);
-//        System.out.println(d);
-//        System.out.println("--------------------------------------------------");
-//        k.doesDamage(d);
-//        System.out.println("--------------------------------------------------");
-//        d.doesDamage(k);
-//        System.out.println("--------------------------------------------------");
-//        k.doesDamage(d);
-//        System.out.println("--------------------------------------------------");
-//        d.doesDamage(k);
-//        System.out.println("--------------------------------------------------");
-//        k.doesDamage(d);
-//        System.out.println("--------------------------------------------------");
-//        d.doesDamage(k);
-//        System.out.println("--------------------------------------------------");
-//
-//        s.readLine();
     }
 }
